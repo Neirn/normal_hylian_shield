cc40722977f6d7fb426b7702dd813baf0a02b8bb
Neirn <neirny@gmail.com>
2021-07-07 09:47:33 -0700

    Version Bump, replaced display list builder internal buffer with a smart buffer

--- src/normal_hylian_shield/DisplayListBuilder.ts
+++ src/normal_hylian_shield/DisplayListBuilder.ts
@@ -1,42 +1,39 @@
-
+import { SmartBuffer } from 'smart-buffer'
 
 export class DisplayListBuilder {
 
-    private dlBuf = Buffer.alloc(0);
-
+    private dlBuf = new SmartBuffer();
     pushDE01(addr: number) {
         let command = Buffer.alloc(8);
         command.writeUInt32BE(0xDE010000);
         command.writeUInt32BE(addr, 4);
-        this.dlBuf = Buffer.concat([this.dlBuf, command]);
+        this.dlBuf.writeBuffer(command);
     }
 
     pushDE00(addr: number) {
         let command = Buffer.alloc(8);
         command.writeUInt32BE(0xDE000000);
         command.writeUInt32BE(addr, 4);
-        this.dlBuf = Buffer.concat([this.dlBuf, command]);
+        this.dlBuf.writeBuffer(command);
     }
 
     pushMtx(addr: number) {
         let command = Buffer.alloc(8);
         command.writeUInt32BE(0xDA380000);
         command.writeUInt32BE(addr, 4);
-        this.dlBuf = Buffer.concat([this.dlBuf, command]);
+        this.dlBuf.writeBuffer(command);
     }
 
     popMtx() {
-        this.dlBuf = Buffer.concat([this.dlBuf, Buffer.from("D838000200000040", 'hex')]);
+        this.dlBuf.writeBuffer(Buffer.from("D838000200000040", 'hex'));
     }
 
     pushDF() {
-        this.dlBuf = Buffer.concat([this.dlBuf, Buffer.from("DF00000000000000", 'hex')]);
+        this.dlBuf.writeBuffer(Buffer.from("D838000200000040", 'hex'));
     }
 
     getDisplayList() {
-        let buf = Buffer.alloc(this.dlBuf.byteLength);
-        this.dlBuf.copy(buf);
-        return buf;
+        return Buffer.from(this.dlBuf.toBuffer);
     }
 
     size() {
